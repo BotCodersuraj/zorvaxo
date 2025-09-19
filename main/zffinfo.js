168a7044390d73380755715f73fbf0b8cb9a4447
BotCodersuraj <moharsingh7221@gmail.com>
2025-09-19 09:39:15 +0530

    Update zffinfo.js

--- main/zffinfo.js
+++ main/zffinfo.js
@@ -1,26 +1,7 @@
-// File: 
-
-export default async function handler(req, res) {
-  try {
-    const apiUrl = `https://danger-info-alpha.vercel.app/accinfo?uid=8080519000&key=DANGERxINFO`;
-
-    // Dusre API se data fetch
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-
-    // Apni API me wahi data bhejna
-    res.status(200).json({
-      success: true,
-      from: "danger-info-alpha",
-      uid: "8080519000",
-      data: data
-    });
-
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Error fetching data",
-      error: error.message
-    });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    success: true,
+    message: "ZFF Info API working 🚀",
+    owner: "Suraj bhai"
+  });
 }
